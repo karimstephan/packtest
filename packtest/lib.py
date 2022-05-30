56631af1bce137c94ef33f534772813b65088338
Karim Stephan <karim.stephan@gmail.com>
2022-05-30 15:29:56 +0800

    no idea

---
+++ packtest/lib.py
@@ -0,0 +1,6 @@
+def do_u_understand(input):
+    if input == "Yes":
+        return "No you don't"
+    elif input == "No":
+        return "I don't understand either"
+    return "It's a yes or no question"
